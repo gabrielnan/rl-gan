c90573d6d9aab075dc14647def2d457499b1db34
Gabriel Nakajima An <gabriel.nakajima.an@gmail.com>
2017-03-03 12:36:40 -0500

    added args functionality to run file and added .pyc to ignore file

--- run.py
+++ run.py
@@ -1,28 +1,42 @@
+import argparse
 from ast import literal_eval as make_tuple
 from tqdm import tqdm
 import cartpoleMod
 import gym
 import numpy as np
+import gan
 
-exp = ['CartPole-v0','Pendulum-v0', 'MountainCar-v0', 'SpaceInvaders-v0']
+exp = ['CartPole-v0', 'Pendulum-v0', 'MountainCar-v0', 'SpaceInvaders-v0']
 
-def main():
-    filename = 'dataset.csv'
-    data = read(filename)
+
+def main(args):
+    data = read(args.data)
     sample = data[0]
-    env = cartpoleMod.CartPoleModEnv()
+    env = gym.make(args.env)
+    create_set_state_method(env.__class__)
 
     env.reset()
     total_error = compute_error(env, sample)
     print(total_error)
 
+
+def create_set_state_method(cls):
+    def set_state(self, state):
+        self.reset()
+        if self.state.shape == state.shape:
+            self.state = state
+        else:
+            cls.logger.error('Parameter state does not have same dimensions as this environment state.')
+    cls.set_state = set_state
+
+
 def main_record():
     record()
 
-def create_GAN():
 
+def get_generator(data):
+    return gan.get_trained_generator(data)
 
-    return None
 
 def compute_error(env, sample):
     initial_state = sample[0, :-1]
@@ -38,6 +52,7 @@ def compute_error(env, sample):
         total_error += state_error(env.state, state)
         action = int(step[-1])
     return total_error
+
 
 def state_error(true_state, pred_state):
     return np.abs(true_state - pred_state)
@@ -68,6 +83,16 @@ def read(filename):
         header = file.readline()
     return data.reshape(make_tuple(header[2:]))
 
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='CartPole-v0')
+    parser.add_argument('--batch_size', type=int)
+    parser.add_argument('--data', type=str, default='dataset.csv')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    main()
+    args = get_args()
+    main(args)
     # main_record()
