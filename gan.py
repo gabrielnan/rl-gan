fixed error calculation bug in round actions function

gan.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from keras.models import Sequential
 from keras.layers import Dense, Reshape, BatchNormalization, Convolution1D, UpSampling2D, Convolution2D, UpSampling1D
-from keras.layers.core import Activation, Flatten
+from keras.layers.core import Activation, Flatten, Lambda
 from keras.optimizers import SGD, Adam
 
 # constants
@@ -25,6 +25,7 @@ def generator_conv_model(nb_steps, state_dim, noise_dim):
     model.add(Convolution2D(1, conv_length, 1, border_mode='same'))
     model.add(Activation('linear'))
     model.add(Reshape((nb_steps, state_dim)))
+    model.add(Lambda(round_actions))
     # print model.summary()
 
     optimizer = SGD(lr=0.005, momentum=0.9, nesterov=True)
@@ -46,10 +47,16 @@ def generator_dense_model(nb_steps, state_dim, noise_dim):
     model.add(Dense(nb_steps * state_dim))
     model.add(Activation('linear'))
     model.add(Reshape((nb_steps, state_dim)))
+    # model.add(Lambda(round_actions))
 
     opt = Adam(lr=1e-4)
     model.compile(loss='binary_crossentropy', optimizer=opt)
     return model
+
+
+def round_actions(x):
+    x[:, -1] = np.round(1 / (1 + np.exp(x[:, -1])))
+    return x.astype(int)
 
 
 def discriminator_conv_model(nb_steps, state_dim):
@@ -136,7 +143,7 @@ def pre_train_discriminator(data, generator, discriminator, batch_size=50):
     discriminator.fit(in_batch, out_batch, nb_epoch=1, batch_size=128)
 
 
-def get_trained_generator(data, noise_dim, batch_size=50, nb_epoch=5000, plt_freq=100, save_freq=100, use_old=False,
+def get_trained_generator(data, noise_dim, batch_size=50, nb_epoch=1200, plt_freq=100, save_freq=100, use_old=False,
                           conv=True):
     # define loss lists
     losses = {'d': [], 'g': []}
@@ -191,7 +198,8 @@ def get_trained_generator(data, noise_dim, batch_size=50, nb_epoch=5000, plt_fre
             # if epoch % plt_freq == plt_freq - 1:
             #     plot_loss(losses)
 
-    plot_loss(losses, data.shape, conv)
+    if not use_old:
+        plot_loss(losses, data.shape, conv)
     save(generator, discriminator, nb_epoch, data.shape, conv)
     return generator
 
@@ -219,7 +227,7 @@ def save(generator, discriminator, epoch, shape, conv):
 class Generator(object):
     def __init__(self, data, noise_dim):
         self.noise_dim = noise_dim
-        self.generator = get_trained_generator(data, noise_dim, use_old=True, conv=True)
+        self.generator = get_trained_generator(data, noise_dim, use_old=True, conv=False)
 
     def generate(self, size):
         noise = get_noise(size, self.noise_dim)

run.py
@@ -23,10 +23,13 @@ def main(args):
 
     env.reset()
     total_error = dataset_error(env, samples)
-    print('Avg Error:', total_error)
+    print('Avg Error:', list(total_error))
+    # print(samples[0])
 
-    total_error_random = dataset_error(env, np.random.uniform(-1, 1, size=samples.shape))
-    print('Avg Random Error:', total_error_random)
+    random_samples = np.random.uniform(-1, 1, size=samples.shape)
+    random_total_error = dataset_error(env, random_samples)
+    print('Avg Random Error:', list(random_total_error))
+    # print(random_samples[0])
 
 
 def create_set_state_method(cls):
@@ -49,14 +52,14 @@ def dataset_error(env, data):
 
 
 def round_actions(sample):
-    sample[:, -1] = np.round(1 / (1 + np.exp(sample[:, 1])))
-    return sample.astype(int)
+    sample[:, -1] = np.round(1 / (1 + np.exp(sample[:, -1])))
+    return sample
 
 
 def sample_error(env, sample):
     initial_state = sample[0, :-1]
     total_error = np.zeros(initial_state.shape)
-    action = sample[0, -1]
+    action = int(sample[0, -1])
     env.set_state(initial_state)
     # print(env.state, '=====', initial_state)
 
@@ -64,9 +67,10 @@ def sample_error(env, sample):
         env.step(action)
         state = step[:-1]
         # print(env.state, '\n', state, '\n\n')
-        total_error += state_error(env.state, state)
-        action = step[-1]
-    return total_error / len(sample[1:])
+        step_error = state_error(env.state, state)
+        total_error += step_error
+        action = int(step[-1])
+    return total_error
 
 
 def state_error(true_state, pred_state):